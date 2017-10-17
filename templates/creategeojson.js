ec17adec8749fb148a281de344b581ce9d01661c
Ray725 <Ray725@users.noreply.github.com>
2017-10-17 19:33:13 -0400

    fixed sleep module installation

--- templates/creategeojson.js
+++ templates/creategeojson.js
@@ -24,18 +24,26 @@ var csvFile = fs.readFile('Opiod Deaths by County 1999-2015.csv', function(err, 
   var county_key = {};
 
   // for each county make geocoding request
-  for(var k = 0; k < counties.length; k++) {
-    var website_string = `https://maps.googleapis.com/maps/api/geocode/json?address=${counties[0].slice(0,-1).substr(1)}&key=AIzaSyCYDdjxFPVgyCik1xIv4ZK-Z8OU-nSkbTo`;
+  for(var k = 0; k < 30; k++) {
+    var format_name = counties[k].slice(0,-1).substr(1);
+    var website_string = `https://maps.googleapis.com/maps/api/geocode/json?address=${format_name}&key=AIzaSyCYDdjxFPVgyCik1xIv4ZK-Z8OU-nSkbTo`;
     request(website_string, function(err, res, body) {
-      if(err) {
-        console.log(err);
+      if(err) { console.log(err); }
+      var htmlJSON = JSON.parse(body);
+      try {
+        county_key.format_name = htmlJSON.results[0].geometry.location;
+        console.log(county_key);
+      } catch(error) {
+        console.log(`${k} position undefined in counties list`);
       }
-      console.log(body);
     });
+    //console.log(county_key);
+    /*
     // make sure we don't pass API allowed calls
-    if(k % 50 === 0) {
+    if(k % 50 === 0 && k > 0) {
       sleep.sleep(1);
     }
+    */
   }
 
   // create geoJSON
