e156a0b304c6269e7c277adf556d9d88eb4faf27
Ray725 <Ray725@users.noreply.github.com>
2017-10-19 16:23:26 -0400

    got connecticut to work

--- templates/creategeojson.js
+++ templates/creategeojson.js
@@ -1,8 +1,11 @@
 var fs = require('fs')
 var request = require('request');
 var sleep = require('sleep');
+var county_key = {};
+var counties = [];
 
-var csvFile = fs.readFile('Opiod Deaths by County 1999-2015.csv', function(err, data) {
+
+fs.readFile('Opiod Deaths by County 1999-2015.csv', function(err, data) {
   if (err) {
     throw err;
   }
@@ -13,38 +16,56 @@ var csvFile = fs.readFile('Opiod Deaths by County 1999-2015.csv', function(err, 
   }
 
   // first get a list of all unique counties
-  var counties = [];
-  for(var j = 1; j < data_array.length; j++) {
+  for (var j = 1; j < data_array.length; j++) {
     // if the county isn't in the list of counties
-    if(counties.indexOf(data_array[j][3]) === -1) {
+    if (counties.indexOf(data_array[j][3]) === -1) {
       counties.push(data_array[j][3]);
     }
   }
 
-  var county_key = {};
+  geocoderequest(counties);
+});
 
-  // for each county make geocoding request
-  for(var k = 0; k < 30; k++) {
-    var format_name = counties[k].slice(0,-1).substr(1);
-    var website_string = `https://maps.googleapis.com/maps/api/geocode/json?address=${format_name}&key=AIzaSyCYDdjxFPVgyCik1xIv4ZK-Z8OU-nSkbTo`;
-    request(website_string, function(err, res, body) {
-      if(err) { console.log(err); }
-      var htmlJSON = JSON.parse(body);
+function geocoderequest(countieslist) {
+  // for each county make geocoding request and add to dictionary/object
+  for (var k = 0; k < countieslist.length; k++) {
+    if(k === 3133) {
+      continue;
+    }
+    console.log(`On number ${k} out of ${countieslist.length}`);
+    try {
+      var county_name = countieslist[k].slice(0, -1).substr(1);
+    } catch(error) {
+      continue;
+    }
+    var website_string = `https://maps.googleapis.com/maps/api/geocode/json?address=${county_name}&key=AIzaSyCYDdjxFPVgyCik1xIv4ZK-Z8OU-nSkbTo`;
+    getlocation(county_name, website_string, printfunction, function(htmlJSON, county, printfunc) {
       try {
-        county_key.format_name = htmlJSON.results[0].geometry.location;
-        console.log(county_key);
-      } catch(error) {
+        county_key[county] = htmlJSON.results[0].geometry.location;
+      } catch (error) {
         console.log(`${k} position undefined in counties list`);
       }
+      printfunc();
     });
-    //console.log(county_key);
-    /*
+
     // make sure we don't pass API allowed calls
     if(k % 50 === 0 && k > 0) {
       sleep.sleep(1);
     }
-    */
-  }
 
-  // create geoJSON
-});
+  }
+}
+
+function getlocation(county, url, printfunc, callback) {
+  request(url, function(err, res, body) {
+    if (err) {
+      console.log(err);
+    }
+    var htmlJSON = JSON.parse(body);
+    callback(htmlJSON, county, printfunc);
+  });
+}
+
+function printfunction() {
+  console.log(county_key);
+}
